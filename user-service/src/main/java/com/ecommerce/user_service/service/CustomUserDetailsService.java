d384a29b561631ad78e32dd4ad183158509d5e5b
Eby Mathew <ebykannamkulathu@gmail.com>
2025-07-04 15:34:07 +0530

    Add CustomUserDetailsService

--- user-service/src/main/java/com/ecommerce/user_service/service/CustomUserDetailsService.java
+++ user-service/src/main/java/com/ecommerce/user_service/service/CustomUserDetailsService.java
@@ -1,4 +1,37 @@
 package com.ecommerce.user_service.service;
 
-public class CustomUserDetailsService {
+import com.ecommerce.user_service.entity.User;
+import com.ecommerce.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+
+    }
+
+
+
 }
